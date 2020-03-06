878255763e8c3350edae07ca0c735adedd497e5a
Florian Eiden <florian.eiden@fleid.fr>
2020-03-06 15:08:07 -0800

    Add cove-coverage shortcut

--- asa.unittest.tests/Start-AutRun.Tests.ps1
+++ asa.unittest.tests/Start-AutRun.Tests.ps1
@@ -6,6 +6,7 @@ $moduleName = Split-Path $moduleRoot -Leaf #Extract the module name from above
 Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force
 
 #############################################################################################################
+# Invoke-Pester .\Start-AutRun.Tests.ps1 -CodeCoverage .\..\asa.unittest\public\Start-AutRun.ps1
 
 
 Describe "Start-AutRun parameter asaNugetVersion" {
